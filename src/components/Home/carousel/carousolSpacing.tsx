60c14874eac0364d110e280452cbb49c3ec46b61
Alamin Mustafa Rahim <asaduzzamanalamin112809@gmail.com>
2026-02-20 21:01:30 +0600

    in slider image ad done

--- src/components/Home/carousel/carousolSpacing.tsx
+++ src/components/Home/carousel/carousolSpacing.tsx
@@ -15,16 +15,16 @@ import {
 export function CarouselSpacing() {
   // MODIFIED: add your own image paths here inside the array (place images in the /public folder)
   const images = [
-    "/img1.jpg",
-    "/img2.jpg",
-    "/img3.jpg",
-    "/img4.jpg",
-    "/img5.jpg",
-    "/img6.jpg",
-    "/img7.jpg",
-    "/img8.jpg",
-    "/img9.jpg",
-    "/img10.jpg",
+    "/images/istockphoto-2247792230-2048x2048.webp",
+    "/images/pexels-cottonbro-5483071.jpg",
+    "/images/pexels-jeshoots-com-147458-714699.jpg",
+    "/images/pexels-karola-g2-5775.jpg",
+    "/images/pexels-pixabay-256417.jpg",
+    "/images/pexels-thisisengineering-3862130.jpg",
+    "/images/photo-1554475901-4538ddfbccc2.avif",
+    "/images/photo-1565022536102-f7645c84354a.avif",
+    "/images/photo-1649180556628-9ba704115795.avif",
+    "/images/premium_photo-1661715935533-507e796866e5.avif",
   ];
 
   return (
